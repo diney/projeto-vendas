a639eff620818d974e1a09f4bceb59920623759f
Diney <diney.dom@gmail.com>
2021-09-07 16:51:30 -0300

    Projeto criado

---
+++ backend/src/test/java/com/diney/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diney.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
