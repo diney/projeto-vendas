efc131a78307fa0fc38ef8d3763c0d0d0e2fc627
Diney <diney.dom@gmail.com>
2021-09-12 16:37:54 -0300

    Env var

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -12,7 +12,7 @@ const Dashboard = () => {
                 <h1 className="text-primary py-3" >Dashboard de Vendas</h1>
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5>Todas vendas</h5>
+                        <h5>Taxa de Sucesso(%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
